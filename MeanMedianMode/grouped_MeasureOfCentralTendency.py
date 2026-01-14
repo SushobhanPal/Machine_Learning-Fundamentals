9690365cd22a050cc974f2cb7e5e2d67cf52ec80
SushobhanPal <sushobhanpalofficial@gmail.com>
2026-01-14 19:56:43 +0530

    Combined Mean Median Mode to One program

--- MeanMedianMode/grouped_MeasureOfCentralTendency.py
+++ MeanMedianMode/grouped_MeasureOfCentralTendency.py
@@ -14,16 +14,25 @@ for i in range(n):
     freq.append(c)
     midpoint.append((a+b)/2)
 
-
+#Mean
+assumed_mean=midpoint[n//2]
+h=class_higher[0]-class_lower[0]
 
 sum1=0
-
+sum2=0
 for i in range(n):
-    sum1+=freq[i]
-    c_freq.append(sum1)
+    sum1+=freq[i]*((midpoint[i]-assumed_mean)/h)
+    sum2+=freq[i]
+    c_freq.append(sum2)
 
+mean=assumed_mean + (h * (sum1/sum2))
+
+print("Mean of the given data is: ",mean)
+
+
+# Median
 for i in range(n):
-    if(c_freq[i]>=(sum1/2)):
+    if(c_freq[i]>=(sum2/2)):
         median_class=i
         break
 
@@ -31,9 +40,19 @@ l=class_lower[median_class]
 f=freq[median_class]
 cf=0 if median_class==0 else c_freq[median_class-1]
 
-h=class_higher[median_class]-class_lower[median_class]
-
-median = l+(h*(((sum1/2)-cf)/f))
+median = l+(h*(((sum2/2)-cf)/f))
 
 print("Median is ",median)
+    
+#Mode
+f0=max(freq)
+modal_class=freq.index(f0)
+l_m=class_lower[modal_class]
+f1=freq[modal_class-1] if modal_class>0 else 0
+f2=freq[modal_class+1] if modal_class<n-1 else 0
 
+
+
+mode = l_m+(((f0-f1)/(2*f0-f1-f2))*h)
+
+print("Mode: ",mode)
--- MeanMedianMode/ungrouped_MeasureOfCentralTendency.py
+++ MeanMedianMode/ungrouped_MeasureOfCentralTendency.py
@@ -2,7 +2,29 @@ li=[]
 n=int(input("enter how many datas to be entered\n"))
 for i in range(n):
     li.append(int(input("enter element")))
+
 li.sort()
+#mean
+
+sum1=0
+for i in range(n):
+    sum1+=li[i]
+mean =sum1/n
+
+print("mean is ", mean)
+
+
+#median
+
+if (n%2==0):
+    median = (li[int(n/2)]+li[int((n/2)-1)])/2
+else:
+    median = li[int((n+1)/2)]
+    
+print("Medain is ", median)
+
+#mode
+
 li_count=[]
 li_mode=[]
 
@@ -19,8 +41,6 @@ maximum=max(li_count)
 mode=li_mode[li_count.index(maximum)]
 
 print("Mode is  is ",mode)
-print(maximum)
-print(li_mode)
-print(li_count)
+
 
     
